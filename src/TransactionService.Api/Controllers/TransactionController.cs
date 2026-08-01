dcfc34f1d755b3bc2bf96b2f37d095fcc1d016e4
yasemin.albayrak <yasemin.albayrak@mentormate.com>
2026-08-01 23:06:41 +0300

    Organized solution structure

---
+++ src/TransactionService.Api/Controllers/TransactionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TransactionService.Api.Controllers
+{
+    public class TransactionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/TransactionService.Domain/Enums/TransactionType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransactionService.Domain.Enums
+{
+    public enum TransactionType
+    {
+    }
+}
